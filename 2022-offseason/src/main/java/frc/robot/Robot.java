3f892859a8b5bb1f1a3a8e5166f4b5ef4b971a9b
Mel_sacchi <melissasacchi@hotmail.com>
2022-01-11 17:27:45 -0700

    motor code

--- 2022-offseason/src/main/java/frc/robot/Robot.java
+++ 2022-offseason/src/main/java/frc/robot/Robot.java
@@ -25,9 +25,9 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    m_robotContainer = new RobotContainer();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
   }
 
   /**
@@ -70,6 +70,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
